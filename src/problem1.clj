334b377e5f12de620940f45c3e52194e3e742152
Ñ <infinito84@gmail.com>
2024-02-09 15:25:57 -0500

    test: configured test and added problem1 test

---
+++ src/problem1.clj
@@ -0,0 +1,5 @@
+(ns problem1 (:gen-class))
+
+(defn get-valid-invoice-items [invoice]
+  [])
+
---
+++ test/problem1_test.clj
@@ -0,0 +1,21 @@
+(ns problem1-test
+  (:require [clojure.test :refer :all]
+            [problem1 :as problem1]
+            [clojure.edn :refer [read-string]]))
+
+
+
+(deftest thread-last-operator
+  (testing "checking the result of invoice.edn"
+    (let [invoice (read-string (slurp "invoice.edn"))
+          invoice-items (problem1/get-valid-invoice-items invoice)]
+      ;; following tests are based on manual inspection on invoice.edn file
+      (is (= 2 (count invoice-items)))
+      (is (= 19
+             (get-in invoice-items
+                     [:taxable/taxes 0 :tax/rate])))
+      (is (= "ii3" (get invoice-items :invoice-item/id)))
+      (is (= 1
+             (get-in invoice-items
+                     [:retentionable/retentions 0 :retention/rate])))
+      (is (= "ii4" (get invoice-items :invoice-item/id))))))
