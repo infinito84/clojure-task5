d265b71bab50a800eee6d4a6c3072c288d011c33
Ñ <infinito84@gmail.com>
2024-02-09 19:09:18 -0500

    feat: added problem2 solution

--- src/problem2.clj
+++ src/problem2.clj
@@ -1,3 +1,27 @@
-(ns problem2)
+(ns problem2
+  (:require [clojure.data.json :refer [read-str]]
+            [clj-time.format :as f]))
 
-(defn parse-from-json [jsonFile] {})
+(defn parse-from-json
+  [jsonFile]
+  (let [jsonData (read-str (slurp jsonFile))]
+    (into {} [[:invoice/issue-date
+               (f/parse
+                (f/formatter "dd/MM/yyyy")
+                (get-in jsonData ["invoice" "issue_date"]))]
+              [:invoice/customer
+               (into {} [[:customer/name (get-in jsonData ["invoice" "customer" "company_name"])]
+                         [:customer/email (get-in jsonData ["invoice" "customer" "email"])]])]
+              [:invoice/items (into [] (map
+                                        #(into {} [[:invoice-item/price (get % "price")]
+                                                   [:invoice-item/quantity (get % "quantity")]
+                                                   [:invoice-item/sku (get % "sku")]
+                                                   [:invoice-item/taxes (into [] (map
+                                                                                  (fn [tax] (into {} [[:tax/category :iva]
+                                                                                                      [:tax/rate (double (get tax "tax_rate"))]]))
+                                                                                  (get % "taxes")))]])
+                                        (get-in jsonData ["invoice" "items"])))]])))
+
+
+
+
